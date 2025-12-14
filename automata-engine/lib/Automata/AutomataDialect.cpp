62f0f8bd081bd6d38c0bb04aced1f55ac2f608b8
Mord Mora <100853548+mordmora@users.noreply.github.com>
2025-12-14 02:28:23 -0500

    Clean up comments in AutomataDialect.cpp
    Removed commented sections for dialect and operations.

--- automata-engine/lib/Automata/AutomataDialect.cpp
+++ automata-engine/lib/Automata/AutomataDialect.cpp
@@ -6,21 +6,11 @@
 using namespace mlir;
 using namespace mlir::automata;
 
-// ==========================================================
-// SECCIÓN 1: Implementación del DIALECTO
-// ==========================================================
 #include "automata/AutomataDialect.cpp.inc"
 
-// ==========================================================
-// SECCIÓN 2: Implementación de las OPERACIONES
-// ==========================================================
-// ¡OJO! Este define DEBE ir justo antes del include de Ops.cpp.inc
 #define GET_OP_CLASSES
 #include "automata/AutomataOpsImpl.inc"
 
-// ==========================================================
-// SECCIÓN 3: Inicialización
-// ==========================================================
 void AutomataDialect::initialize() {
     addOperations<
         #define GET_OP_LIST
